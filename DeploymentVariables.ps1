265c73c21b0b228981b47587f6f397c00b40a237
tim110 <33260001+tim110@users.noreply.github.com>
2022-07-30 23:53:24 +0200

    Update DeploymentVariables.ps1

--- DeploymentVariables.ps1
+++ DeploymentVariables.ps1
@@ -21,7 +21,7 @@ Write-Warning "That didn't work because I haven't coded it yet!"
 
 #Start OSDCloud ZTI the RIGHT way
 Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
-Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI
+Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H2 -OSEdition Professional -ZTI
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
 Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
