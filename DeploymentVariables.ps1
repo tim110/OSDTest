02efc60aea326a87361d09e027a5db7d4fab681b
tim110 <33260001+tim110@users.noreply.github.com>
2024-11-24 19:00:18 +0100

    Update DeploymentVariables.ps1

--- DeploymentVariables.ps1
+++ DeploymentVariables.ps1
@@ -1,4 +1,4 @@
-Write-Host  -ForegroundColor Cyan "Starting SeguraOSD's Custom OSDCloud ..."
+Write-Host  -ForegroundColor Cyan "Starting custom OSDCloud"
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
@@ -8,24 +8,20 @@ if ((Get-MyComputerModel) -match 'Virtual') {
 }
 
 #Make sure I have the latest OSD Content
-Write-Host  -ForegroundColor Cyan "Updating the awesome OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
 Install-Module OSD -Force
 
-Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
 Import-Module OSD -Force
 
-#TODO: Spend the time to write a function to do this and put it here
-Write-Host  -ForegroundColor Cyan "Ejecting ISO"
-Write-Warning "That didn't work because I haven't coded it yet!"
-#Start-Sleep -Seconds 5
-
 #Start OSDCloud ZTI the RIGHT way
-Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
-Start-OSDCloud -OSLanguage nl-nl -OSBuild 21H2 -OSEdition Enterprise -ZTI
+Write-Host  -ForegroundColor Cyan "Starting installation"
+StartOSDCloud -OSName 'Windows 11 24H2 x64' -OSEdition Enterprise -OSLanguage nl-nl -OSLicense Retail -ZTI"
 
 #Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
-Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
-Write-Warning "I'm not sure of what to put here yet"
+Write-Host  -ForegroundColor Cyan "Copying config files to image"
+Copy-Item -Path "D:\OSDCloud\PowerShell\Offline\Scripts*" -Destination "C:\Program Files\PowerShell\Scripts" -Recurse
+Copy-Item -Path "D:\OSDCloud\PowerShell\Offline\Scripts*" -Destination "C:\Program Files\PowerShell\Scripts" -Recurse
 
 #Restart from WinPE
 Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
